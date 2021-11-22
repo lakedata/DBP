806274c11fc25548e2ecfcccbac0e8ba9c56b237
juri <jr4426@naver.com>
2021-11-23 00:56:24 +0900

    [ADD] logoutcontroller

---
+++ src/main/java/controller/user/LogoutController.java
@@ -0,0 +1,22 @@
+package controller.user;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import controller.Controller;
+
+public class LogoutController implements Controller{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		
+		HttpSession session = request.getSession();
+		session.invalidate();
+		
+		return "redirect/어디로?";
+	}
+	
+
+}
