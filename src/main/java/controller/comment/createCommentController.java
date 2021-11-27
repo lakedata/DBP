748e46f108b982d131b31d372e28b31995afaa65
1201silver <1201silver@naver.com>
2021-11-27 15:54:09 +0900

    comment 수정

--- src/main/java/controller/comment/createCommentController.java
+++ src/main/java/controller/comment/createCommentController.java
@@ -20,7 +20,8 @@ public class createCommentController implements Controller {
 		
 		try {
 			CommentManager commMan = CommentManager.getInstance();
-			commMan.create(comm);			
+			commMan.create(comm);		
+			
 			return "redirection:/policy/view";
 		} catch (Exception e) {
 			request.setAttribute("createCommentFailed", true);
