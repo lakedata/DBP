465c1674567b653b4840f085d55273c5609d692c
1201silver <1201silver@naver.com>
2021-11-28 01:14:11 +0900

    [RE]

--- src/main/java/controller/user/LoginController.java
+++ src/main/java/controller/user/LoginController.java
@@ -1,4 +1,4 @@
-rpackage controller.user;
+package controller.user;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,26 +20,26 @@ public class LoginController implements Controller {
 		String password = request.getParameter("password");
 		
 		try {
-			// 占쏜델울옙 占싸깍옙占쏙옙 처占쏙옙占쏙옙 占쏙옙占쏙옙
+			// �뜝�룣�뜽�슱�삕 �뜝�떥源띿삕�뜝�룞�삕 泥섇뜝�룞�삕�뜝�룞�삕 �뜝�룞�삕�뜝�룞�삕
 			UserManager manager = UserManager.getInstance();
 			manager.login(userId, password);
 			
 			User user = userDAO.findUser(userId);
 			if(user == null) {
-				throw new UserNotFoundException(userId + "�뒗 議댁옱�븯吏� �븡�뒗 �븘�씠�뵒�엯�땲�떎. ");
+				throw new UserNotFoundException(userId + "占쎈뮉 鈺곕똻�삺占쎈릭筌욑옙 占쎈륫占쎈뮉 占쎈툡占쎌뵠占쎈탵占쎌뿯占쎈빍占쎈뼄. ");
 			}
 			if(!user.matchPassword(password)) {
-				throw new PasswordMismatchException("鍮꾨�踰덊샇媛� �씪移섑븯吏� �븡�뒿�땲�떎.");
+				throw new PasswordMismatchException("�뜮袁⑨옙甕곕뜇�깈揶쏉옙 占쎌뵬燁살꼹釉�筌욑옙 占쎈륫占쎈뮸占쎈빍占쎈뼄.");
 			}
 	
-			//�꽭�뀡�뿉 userid ���옣 
+			//占쎄쉭占쎈�∽옙肉� userid 占쏙옙占쎌삢 
 			HttpSession session = request.getSession();
             session.setAttribute(UserSessionUtils.USER_SESSION_KEY, userId);
             
             return "redirect:/user/list";			
 		} catch (Exception e) {
-			/* UserNotFoundException占싱놂옙 PasswordMismatchException 占쌩삼옙 占쏙옙
-			 * 占쌕쏙옙 login form占쏙옙 占쏙옙占쏙옙悶占쏙옙占� 占쏙옙占쏙옙占싹곤옙 占쏙옙占쏙옙 占쌨쇽옙占쏙옙占쏙옙 占쏙옙占�
+			/* UserNotFoundException�뜝�떛�냲�삕 PasswordMismatchException �뜝�뙥�궪�삕 �뜝�룞�삕
+			 * �뜝�뙐�룞�삕 login form�뜝�룞�삕 �뜝�룞�삕�뜝�룞�삕�궣�뜝�룞�삕�뜝占� �뜝�룞�삕�뜝�룞�삕�뜝�떦怨ㅼ삕 �뜝�룞�삕�뜝�룞�삕 �뜝�뙣�눦�삕�뜝�룞�삕�뜝�룞�삕 �뜝�룞�삕�뜝占�
 			 */
             request.setAttribute("loginFailed", true);
 			request.setAttribute("exception", e);
