dc3a97bcf4e75792334b084d341528f1e6f28133
1201silver <1201silver@naver.com>
2021-11-30 16:21:00 +0900

    [RE] 충돌 수정

--- src/main/java/model/dao/PostDAO.java
+++ src/main/java/model/dao/PostDAO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package model.dao;
 
 import java.sql.ResultSet;
@@ -14,173 +13,10 @@ public class PostDAO {
 	private JDBCUtil jdbcUtil = null;
 	
 	public PostDAO() {			
-		jdbcUtil = new JDBCUtil();	// JDBCUtil ��ü ����
+		jdbcUtil = new JDBCUtil();	// JDBCUtil 占쏙옙체 占쏙옙占쏙옙
 	}
 	
-	/* �Խñ� ��� */
-	public Post insertPost(Post po) throws SQLException {
-		
-		int generatedKey;
-		
-		String sql = "INSERT INTO Post VALUES (?, ?, ?, ?, ?, ?)";
-		
-		Object[] param = new Object[] {po.getPostNum(), po.getPolicyId(), po.getUserId(), po.getTitle(), po.getWriteDate(), po.getContent()};	
-		
-		jdbcUtil.setSqlAndParameters(sql, param);
-		
-		String key[] = {"postNum"};
-		
-		try {
-			jdbcUtil.executeUpdate();
-			
-			ResultSet rs = jdbcUtil.getGeneratedKeys();
-			if(rs.next()) {
-				generatedKey = rs.getInt(1);
-				po.setPostNum(generatedKey);
-			}
-			return po;
-		} catch (Exception e) {
-			jdbcUtil.rollback();
-			e.printStackTrace();
-		} finally {
-			jdbcUtil.commit();
-			jdbcUtil.close();
-		}
-		
-		return null;
-	}
-	
-	public int updatePost(Post po) throws SQLException{
-	
-		String sql = "UPDATE Post "
-					+ "SET content=? "
-					+ "WHERE postNum=?";
-		
-		String content = po.getContent();
-		
-		if (content.equals("")) content = null;
-		
-		Object[] param = new Object[] {content};
-		jdbcUtil.setSqlAndParameters(sql, param);
-		
-		try {
-			int result = jdbcUtil.executeUpdate();
-			return result;
-		} catch (Exception e) {
-			jdbcUtil.rollback();
-			e.printStackTrace();
-		} finally {
-			jdbcUtil.commit();
-			jdbcUtil.close();
-		}
-		return 0;
-		
-	}
-	
-	public int deletePost(int postNum) throws SQLException {
-		
-		String sql = "DELETE Post "
-					+ "WHERE postNum=?";
-		
-		jdbcUtil.setSqlAndParameters(sql, new Object[] {postNum});
-		
-		try {
-			int result = jdbcUtil.executeUpdate();
-			return result;
-		} catch (Exception e) {
-			jdbcUtil.rollback();
-			e.printStackTrace();
-		} finally {
-			jdbcUtil.commit();
-			jdbcUtil.close();
-		}
-		return 0;
-	}
-	
-	public Post findPost(int postNum) throws SQLException {
-        String sql = "SELECT policyId, userId, title, writeDate, content "
-                 + "FROM Post "
-                 + "WHERE postNum=? ";   
-        
-      jdbcUtil.setSqlAndParameters(sql, new Object[] {postNum});      // JDBCUtil占쏙옙 query占쏙옙 占쏙옙占쏙옙
-      Post post = null;
-      
-      try {
-         ResultSet rs = jdbcUtil.executeQuery();         // query 占쏙옙占쏙옙         
-  
-         if (rs.next()) {
-            post = new Post (
-            		rs.getInt("postNum"),
-            		rs.getInt("policyId"),
-            		rs.getString("userId"),
-            		rs.getString("title"),
-            		rs.getString("writeDate"),
-            		rs.getString("content")
-      		 );  
-         }                    
-         
-      } catch (Exception ex) {
-         ex.printStackTrace();
-      } finally {
-         jdbcUtil.close();      // resource 占쏙옙환
-      }
-      return post;
-   }
-	
-	/* ��ü post ã�� */
-	public List<Post> findPostList() throws SQLException {
-		
-        String sql = "SELECT postNum, policyId, userId, title, writeDate, content "
-     		   + "FROM Post "
-     		   + "ORDER BY postNum";   
-        
-		jdbcUtil.setSqlAndParameters(sql, null);		// JDBCUtil�� query�� ����
-					
-		try {
-			ResultSet rs = jdbcUtil.executeQuery();			// query ����			
-			List<Post> postList = new ArrayList<Post>();	// Community���� ����Ʈ ����
-			while (rs.next()) {
-				Post po = new Post(			// Community ��ü�� �����Ͽ� ���� ���� ������ ����
-						rs.getInt("postNum"),
-						rs.getInt("policyId"),
-						rs.getString("userId"),
-						rs.getString("title"),
-						rs.getString("writeDate"),
-						rs.getString("content"));
-				postList.add(po);				// List�� Community ��ü ����
-			}		
-			return postList;					
-			
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		} finally {
-			jdbcUtil.close();		// resource ��ȯ
-		}
-		return null;
-	}
-	
-	
-}
-=======
-package model.dao;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import model.Policy;
-import model.Post;
-
-public class PostDAO {
-
-	private JDBCUtil jdbcUtil = null;
-	
-	public PostDAO() {			
-		jdbcUtil = new JDBCUtil();	// JDBCUtil ��ü ����
-	}
-	
-	/* �Խñ� ��� */
+	/* 占쌉시깍옙 占쏙옙占� */
 	public Post insertPost(Post po) throws SQLException {
 			
 		int generatedKey;
@@ -265,11 +101,11 @@ public class PostDAO {
                  + "FROM Post "
                  + "WHERE postNum=? ";   
         
-      jdbcUtil.setSqlAndParameters(sql, new Object[] {postNum});      // JDBCUtil占쏙옙 query占쏙옙 占쏙옙占쏙옙
+      jdbcUtil.setSqlAndParameters(sql, new Object[] {postNum});      // JDBCUtil�뜝�룞�삕 query�뜝�룞�삕 �뜝�룞�삕�뜝�룞�삕
       Post post = null;
       
       try {
-         ResultSet rs = jdbcUtil.executeQuery();         // query 占쏙옙占쏙옙         
+         ResultSet rs = jdbcUtil.executeQuery();         // query �뜝�룞�삕�뜝�룞�삕         
   
          if (rs.next()) {
             post = new Post (
@@ -285,43 +121,42 @@ public class PostDAO {
       } catch (Exception ex) {
          ex.printStackTrace();
       } finally {
-         jdbcUtil.close();      // resource 占쏙옙환
+         jdbcUtil.close();      // resource �뜝�룞�삕�솚
       }
       return post;
    }
 	
-	/* ��ü post ã�� */
+	/* 占쏙옙체 post 찾占쏙옙 */
 	public List<Post> findPostList() throws SQLException {
 		
         String sql = "SELECT postNum, policyId, userId, title, writeDate, content "
      		   + "FROM Post "
      		   + "ORDER BY postNum";   
         
-		jdbcUtil.setSqlAndParameters(sql, null);		// JDBCUtil�� query�� ����
+		jdbcUtil.setSqlAndParameters(sql, null);		// JDBCUtil占쏙옙 query占쏙옙 占쏙옙占쏙옙
 					
 		try {
-			ResultSet rs = jdbcUtil.executeQuery();			// query ����			
-			List<Post> postList = new ArrayList<Post>();	// Community���� ����Ʈ ����
+			ResultSet rs = jdbcUtil.executeQuery();			// query 占쏙옙占쏙옙			
+			List<Post> postList = new ArrayList<Post>();	// Community占쏙옙占쏙옙 占쏙옙占쏙옙트 占쏙옙占쏙옙
 			while (rs.next()) {
-				Post po = new Post(			// Community ��ü�� �����Ͽ� ���� ���� ������ ����
+				Post po = new Post(			// Community 占쏙옙체占쏙옙 占쏙옙占쏙옙占싹울옙 占쏙옙占쏙옙 占쏙옙占쏙옙 占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙
 						rs.getInt("postNum"),
 						rs.getInt("policyId"),
 						rs.getString("userId"),
 						rs.getString("title"),
 						rs.getString("writeDate"),
 						rs.getString("content"));
-				postList.add(po);				// List�� Community ��ü ����
+				postList.add(po);				// List占쏙옙 Community 占쏙옙체 占쏙옙占쏙옙
 			}		
 			return postList;					
 			
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		} finally {
-			jdbcUtil.close();		// resource ��ȯ
+			jdbcUtil.close();		// resource 占쏙옙환
 		}
 		return null;
 	}
 	
 	
 }
->>>>>>> branch 'develop' of https://github.com/juri123123/DBP.git
