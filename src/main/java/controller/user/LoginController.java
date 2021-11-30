dd93b21f4bab3ebf29b781ee389970aeca5264e3
1201silver <1201silver@naver.com>
2021-11-30 13:09:26 +0900

    [RE] jsp 이름 수정

--- src/main/java/controller/user/LoginController.java
+++ src/main/java/controller/user/LoginController.java
@@ -26,21 +26,23 @@ public class LoginController implements Controller {
 			
 			User user = userDAO.findUser(userId);
 			if(user == null) {
-				throw new UserNotFoundException(userId + "占쎈뮉 鈺곕똻�삺占쎈릭筌욑옙 占쎈륫占쎈뮉 占쎈툡占쎌뵠占쎈탵占쎌뿯占쎈빍占쎈뼄. ");
+				throw new UserNotFoundException(userId + "user not found");
 			}
 			if(!user.matchPassword(password)) {
-				throw new PasswordMismatchException("�뜮袁⑨옙甕곕뜇�깈揶쏉옙 占쎌뵬燁살꼹釉�筌욑옙 占쎈륫占쎈뮸占쎈빍占쎈뼄.");
+				throw new PasswordMismatchException("password mismatch");
 			}
 	
-			//占쎄쉭占쎈�∽옙肉� userid 占쏙옙占쎌삢 
+			// save user ID in session
 			HttpSession session = request.getSession();
             session.setAttribute(UserSessionUtils.USER_SESSION_KEY, userId);
             
             return "redirect:/user/list";			
 		} catch (Exception e) {
-			/* UserNotFoundException�뜝�떛�냲�삕 PasswordMismatchException �뜝�뙥�궪�삕 �뜝�룞�삕
-			 * �뜝�뙐�룞�삕 login form�뜝�룞�삕 �뜝�룞�삕�뜝�룞�삕�궣�뜝�룞�삕�뜝占� �뜝�룞�삕�뜝�룞�삕�뜝�떦怨ㅼ삕 �뜝�룞�삕�뜝�룞�삕 �뜝�뙣�눦�삕�뜝�룞�삕�뜝�룞�삕 �뜝�룞�삕�뜝占�
+			/* 
+			 	UserNotFoundException or PasswordMismatchException 
+			 	send login form to user, print error message			 
 			 */
+			
             request.setAttribute("loginFailed", true);
 			request.setAttribute("exception", e);
             return "/user/loginForm.jsp";			
