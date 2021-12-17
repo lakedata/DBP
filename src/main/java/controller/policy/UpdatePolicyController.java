2fc24fcc6eec7639995bee769d8dabb43cbdc545
shin-sj <sujung5092@gmail.com>
2021-12-18 01:56:21 +0900

    policy 수정 후 policyDetail로

--- src/main/java/controller/policy/UpdatePolicyController.java
+++ src/main/java/controller/policy/UpdatePolicyController.java
@@ -20,10 +20,10 @@ public class UpdatePolicyController implements Controller{
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
 		PolicyManager polMan = PolicyManager.getInstance();
-		
+		int policyId = Integer.parseInt(request.getParameter("policyId"));
 		if(request.getMethod().equals("GET")) {
 			logger.debug("update controller _ get");
-			int policyId = Integer.parseInt(request.getParameter("policyId"));
+			
 			Policy policy = polMan.findPolicy(policyId);
 			
 			//admin
@@ -34,14 +34,14 @@ public class UpdatePolicyController implements Controller{
 				return "/policy/policyUpdateForm.jsp";
 				
 			}
-			else { // ������ �ƴ� ���
+			else { // 占쏙옙占쏙옙占쏙옙 占싣댐옙 占쏙옙占�
 				logger.debug("update controller _ exce");
 				request.setAttribute("updateFailed", true);
-				request.setAttribute("exception", new IllegalStateException("������ �� �Ϲ� ����ڴ� ��å ������ �Ұ��� �մϴ�."));
+				request.setAttribute("exception", new IllegalStateException("占쏙옙占쏙옙占쏙옙 占쏙옙 占싹뱄옙 占쏙옙占쏙옙渼占� 占쏙옙책 占쏙옙占쏙옙占쏙옙 占쌀곤옙占쏙옙 占쌌니댐옙."));
 				return "/policy/list";
 			}
 		}
-		else { // "POST" -> �Է�(����)�� ��å ���� parameter�� ����
+		else { // "POST" -> 占쌉뤄옙(占쏙옙占쏙옙)占쏙옙 占쏙옙책 占쏙옙占쏙옙 parameter占쏙옙 占쏙옙占쏙옙
 			logger.debug("update controller _ post");
 			
 			Policy updatePol = new Policy(
@@ -62,7 +62,7 @@ public class UpdatePolicyController implements Controller{
 			//test
 			//System.out.println(request.getParameter("name"));
 			polMan.update(updatePol);
-			return "redirect:/policy/list";
+			return "redirect:/policy/view?policyId=" + policyId;
 			
 	
 		}
